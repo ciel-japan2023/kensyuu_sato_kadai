2448b1a737d58db43d6498df6940b7db5fca2a1c
monena999 <139050931+monena999@users.noreply.github.com>
2023-08-02 14:00:33 +0900

    一部内容変更しました(20230802)

--- DATABASE課題/5章/5-5/5-5.sql
+++ DATABASE課題/5章/5-5/5-5.sql
@@ -1,7 +1,15 @@
-SELECT DISTINCT location_name, code_name as item_type_name, item_qty
-FROM em_t_stock_item
-INNER JOIN em_m_location ON em_t_stock_item.location_cd = em_m_location.location_cd
-INNER JOIN em_m_code 
-INNER JOIN em_m_item ON em_m_code.code_value = em_m_item.item_type
-WHERE em_t_stock_item.item_qty >= 30
+SELECT em_m_location.location_name, 
+em_m_code.code_name as item_type_name, 
+SUM(item_qty)
+
+FROM em_t_stock_item 
+
+INNER JOIN em_m_location  ON em_t_stock_item.location_cd = em_m_location.location_cd
+INNER JOIN em_m_item ON em_t_stock_item.item_cd = em_m_item.item_cd
+INNER JOIN em_m_code  ON em_m_item.item_type = em_m_code.code_value 
+AND em_m_code.code_type = 'item_type'
+
+GROUP BY em_m_location.location_name, em_m_code.code_name
+
+HAVING SUM(item_qty) >= 30 
 ORDER BY em_m_location.location_name asc, em_m_code.code_name desc;
